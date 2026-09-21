f4d1b827db210ee46df03d528213498ca1c706fc
Tasos Tsoupakis <117453107+tsoupakis14@users.noreply.github.com>
2026-09-21 12:34:21 +0300

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -5,7 +5,7 @@ window.portfolioData = {
     // { title: "Project name", type: "Website", year: "2026", description: "Short description.", thumbnail: "assets/thumbnails/file.webp", url: "https://...", featured: true }
   ],
   games: [
-    { number: "01", title: "Jump, Jane, Jump!", type: "Endless Runner", description: "Help Jane jump, dodge obstacles, and get as far as she can!", thumbnail: "assets/thumbnails/project-01.png", url: "https://script.google.com/macros/s/AKfycbxYvqOn_1rVEJBAKQqDDFWkPUcgcDd6FUWGheQ0Vsw3Q3c3uxBhibjvF8ZBkAYj0uS_/exec" },
+    { title: "Jump, Jane, Jump!", type: "Endless Runner", description: "Help Jane jump, dodge obstacles, and get as far as she can!", thumbnail: "assets/thumbnails/project-01.png", url: "https://script.google.com/macros/s/AKfycbxYvqOn_1rVEJBAKQqDDFWkPUcgcDd6FUWGheQ0Vsw3Q3c3uxBhibjvF8ZBkAYj0uS_/exec" },
     { number: "02", title: "Football Quiz", type: "Quiz", description: "Test your football knowledge and see how many questions you can get right!", thumbnail: "assets/thumbnails/project-02.png", url: "https://script.google.com/macros/s/AKfycby9VjsFQRscs31nee04lRqo0tWI1c_1_A3zS4QcH-nBpe4niIRMHRKePxg9LFLLVKCTaQ/exec?refresh=9" },
     { number: "03", title: "Fun Quiz", type: "Quiz", description: "A fun quiz packed with surprising questions to test your knowledge and keep you entertained.", thumbnail: "assets/thumbnails/project-03.png", url: "https://script.google.com/macros/s/AKfycbwaOjcbJKPLoguRQKUeBI5E_qTJfhJoyaSOLghtDii1lvXY7mUfu8kOyGA0EU02COz42w/exec" }
   ],
